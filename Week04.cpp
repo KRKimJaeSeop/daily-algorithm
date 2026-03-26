28dd055998b64d59ab53137b3a61dec54a38cbb3
kjs <greenseop@gmail.com>
2026-03-26 20:14:39 +0900

    the 768B is almost done....

--- Week04.cpp
+++ Week04.cpp
@@ -3,32 +3,47 @@
 #include <string>
 #include <stdio.h>
 
-/*
-수가 들어온다.
-수가 1보다 크다면, 그 수를 2로 나눈다.
-홀수라면 가운데 1이 남고, 짝수라면 가운에 0이다.
-*/
 
-int P768B_Recursion(int devidedNum){
-    std::cout<<"devidedNum is"<<devidedNum<<"\n";
-    
-    return 0;
+std::string output="";
+int input_n;
+int input_l;
+int input_r;
+
+
+
+void P768B_Recursion(long long num){
+    //여기가 재귀의 마지막 줄
+    if(num<=1){
+        output+=std::to_string(num);
+        return;
+    }
+    /*
+        필요없는 부분이라면 버린다.
+        이 다음줄이 재귀의 마지막 줄
+    */
+    if((num/2)<=1){
+        
+    }
+
+    //나눠진 수로 각각 실행.
+    P768B_Recursion(num/2);
+    P768B_Recursion(num%2);
+    P768B_Recursion(num/2);
 }
 
 void P768B(){
-    int inputNum;
-    std::cin>>inputNum;
-
-    if(inputNum<=1){
-        std::cout<<"lower than 2\n";
-        return;
+    std::cin>>input_n>>input_l>>input_r;
+        
+    P768B_Recursion(input_n);
+    int score = 0;
+    for (int i = input_l-1; i <= input_r-1; i++)
+    {
+        if(output[i]=='1'){
+            score++;
+        }
     }
-
-    P768B_Recursion(inputNum/2);
-    P768B_Recursion(inputNum%2);
-    P768B_Recursion(inputNum/2);
+    std::cout<<score;
 }
-
 
 
 int main(){
