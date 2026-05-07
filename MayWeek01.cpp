0db8867668ba435aa124b3124be4eef49d3beed3
kjs <greenseop@gmail.com>
2026-05-07 16:47:35 +0900

    0507

--- MayWeek01.cpp
+++ MayWeek01.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <stdio.h>
 #include <algorithm>
+#include <queue>
 
 /*
 n명의 참가자 중,
@@ -25,18 +26,62 @@ void P158A()
 
     for (int i = 0; i < n; i++)
     {
-        if (members[i] >= members[k-1] && members[i] > 0)
+        if (members[i] >= members[k - 1] && members[i] > 0)
         {
             answer++;
         }
+    }
+    std::cout << answer;
+}
 
+/*
+원형 도로에 시계방향으로 1번부터 n번까지 배치됨.
+1번에서 시작해서 m개의 할일.i번째 할일은 반드시 i번째 집에서 완료.
+집에서 집으로 이동할때에는 1시간이 걸린다.
+*/
+void P339B()
+{
+    int n = 0;
+    int m = 0;
+
+    int answer = 0;
+    std::queue<int> q;
+    std::cin >> n >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        int push = 0;
+        std::cin >> push;
+
+        // 작업을 추가한다.
+        q.push(push);
+    }
+
+    int i = 1;
+    // 작업이 남아있는동안 반복한다.
+    while (!q.empty())
+    {
+
+        while (i == q.front())
+        {
+            q.pop();
+        }
+
+        if (q.empty())
+            continue;
+
+        // 한걸음 이동했음
+        answer++;
+        i++;
+        if (i > n)
+            i = 1;
     }
     std::cout << answer;
 }
 
 int main()
 {
-    // std::cout << "======MayWeek01======" << "\n";
-    P158A();
+    //std::cout << "======MayWeek01======" << "\n";
+    P339B();
     return 0;
 }
